110d4a7e0a950d8c9b828c95a388d45e5bb13419
Hp <Hp@DESKTOP-T3T3AH5>
2024-04-27 10:40:46 +0600

    Add project files.

---
+++ AngularEmployee/Models/DbEmployee.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AngularEmployee.Models
+{
+    public class DbEmployee:DbContext
+    {
+        public DbEmployee(DbContextOptions<DbEmployee> op):base(op) { }
+        public DbSet<Employee> Employees { get; set;}
+    }
+}
---
+++ AngularEmployee/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace AngularEmployee.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public DateTime Birthday { get; set; }
+        public DateTime JoinDate { get; set;}
+    }
+}
